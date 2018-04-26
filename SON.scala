c3665c21aad9422c2b34d2254dc1e600a4f61933
fengran_i <fengran_i@didichuxing.com>
2018-04-25 18:10:53 -0700

    There is no 300 dollars here

--- SON.scala
+++ SON.scala
@@ -1,4 +1,4 @@
-////import org.apache.spark.SparkContext
+//import org.apache.spark.SparkContext
 ////import org.apache.spark.SparkContext._
 ////import org.apache.spark.SparkConf
 ////import org.apache.spark.rdd.RDD
